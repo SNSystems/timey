401fd37d9a1623f043533cf3df1c5d5208a68a6b
Paul Bowen-Huggett <paulhuggett@mac.com>
2020-04-15 17:30:04 +0100

    Separate the test driver from the CLI interface.

--- index.js
+++ index.js
@@ -9,7 +9,72 @@ const {promisify} = require ('util');
 
 const csv = require ('./modules/csv');
 const run = require ('./modules/run');
-const timey = require ('./modules/timey.js');
+const timey = require ('./modules/timey');
+
+/**
+ * @param r{run.runner}
+ * @param params{params} The test parameters (values including the linker to time, the max number of
+ *     linkonce and external symbols, the increment, the number of modules.
+ * @param output{string}
+ * @param force{boolean}
+ * @param verbose{boolean}
+ * @result{Promise<>}
+ */
+function do_timings (r, params, output, force, verbose) {
+    const linkonce = params.linkonce;
+    const external = params.external;
+    const increment = params.increment;
+    const num_tasks = Math.floor ((linkonce * external) / (increment * increment));
+
+    let bar;
+    if (!verbose) {
+        bar = new cli_progress.SingleBar ({}, cli_progress.Presets.shades_classic);
+        bar.start (num_tasks);
+    }
+
+    return timey.check_work_directory (r.get_work_dir (), force)
+        .then (() => {
+            return timey.serialize_tasks (Array (num_tasks)
+                .fill (0)
+                .map ((_, index) => {
+                    // Convert an index in the task array to a position in the 2d array of
+                    // external/linkonce combinations to be generated.
+                    const num_external = (Math.floor (index / (linkonce / increment)) + 1) * increment;
+                    const num_linkonce = (index % (linkonce / increment) + 1) * increment;
+
+                    // Time a specific configuration of a particular linker.
+                    return () => timey.single_run (r,
+                        params.linker, // the linker to be timed
+                        params.modules, // the number of modules to generate
+                        num_external, // the number of external symbols to generate
+                        num_linkonce // the number of linkonce symbols to generate
+                    ).then (time => {
+                        if (bar) {
+                            bar.update (index + 1);
+                        }
+                        return [num_external, num_linkonce, time];
+                    });
+                }));
+        })
+        .then (results => {
+            if (bar) {
+                bar.stop ();
+            }
+
+            // Write the results to a file in a format that GnuPlot can display.
+            const write_output = text => promisify (fs.writeFile) (output, text);
+            const write_stdout = text => process.stdout.write (text);
+
+            // Send the results to the chosen output file/stdout.
+            return (output === '-' ? write_stdout : write_output) ('external,linkonce,time\n' + csv.from_array2d (results));
+        })
+        .catch (err => {
+            if (bar) {
+                bar.stop ();
+            }
+            return Promise.reject(err);
+        });
+}
 
 function main () {
     const argv = require ('yargs')
@@ -81,60 +146,26 @@ function main () {
         .group(['bin-dir', 'work-dir', 'repo-name', 'output'], 'Control the location of tools and output:')
         .argv;
 
-    const lomax = argv.linkonce;
-    const extmax = argv.external;
-    const incr = argv.increment;
-    const num_tasks = Math.floor ((lomax * extmax) / (incr * incr));
-
-    // start the progress bar with a total value of 200 and start value of 0
-    let bar;
-    if (!argv.verbose) {
-        bar = new cli_progress.SingleBar ({}, cli_progress.Presets.shades_classic);
-        bar.start (num_tasks);
-    }
-
-    timey.check_work_directory (argv.workDir, argv.force)
-        .then (() => {
-            return timey.serialize_tasks (Array (num_tasks)
-                .fill ()
-                .map ((_, i) => {
-                    const r = run.runner (argv.binDir, argv.workDir, argv.repoName, argv.verbose);
-
-                    // Convert an index in the task array (i) to a
-                    const num_external = (Math.floor (i / (lomax / incr)) + 1) * incr;
-                    const num_linkonce = (i % (lomax / incr) + 1) * incr;
-                    return () => timey.single_run (r, timey.linkers[argv.linker], argv.modules, num_external, num_linkonce)
-                        .then (time => {
-                            if (bar) {
-                                bar.update (i + 1);
-                            }
-                            return [num_external, num_linkonce, time];
-                        });
-                }));
-        })
-        .then (results => {
-            if (bar) {
-                bar.stop ();
-            }
-
-            // Write the results to a file in a format that GnuPlot can display.
-            const write_output = text => promisify (fs.writeFile) (argv.output, text);
-            const write_stdout = text => process.stdout.write (text);
-
-            // Send the results to the chosen output file/stdout.
-            return (argv.output === '-' ? write_stdout : write_output) ('external,linkonce,time\n' + csv.from_array2d (results));
-        })
-        .catch (err => {
-            if (bar) {
-                bar.stop ();
-            }
-            if (argv.debug) {
-                console.trace (err.stack);
-            } else {
-                console.error (err);
-            }
-            process.exitCode = 1;
-        });
+    do_timings (
+        run.runner (argv.binDir, argv.workDir, argv.repoName, argv.verbose),
+        {
+            linker: timey.linkers[argv.linker],
+            linkonce: argv.linkonce,
+            external: argv.external,
+            increment: argv.increment,
+            modules: argv.modules
+        },
+        argv.output,
+        argv.force,
+        argv.verbose
+    ).catch (err => {
+        if (argv.debug) {
+            console.trace (err);
+        } else {
+            console.error (err);
+        }
+        process.exitCode = 1;
+    });
 }
 
 main ();
